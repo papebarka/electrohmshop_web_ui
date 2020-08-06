360ae490c23b5d5aad76866cf448aad90b015c98
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2020-08-06 17:33:30 +0000

    Slider avec splidejs

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+		new Splide( '.splide' ).mount();
+	} );
+
+/*<div class="hero-body">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>*/
